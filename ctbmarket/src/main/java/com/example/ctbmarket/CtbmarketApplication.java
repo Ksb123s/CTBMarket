a2805fe7697adf0c65c922913b4654d7ccd1a26b
sunbeom <sunbum1234@naver.com>
2024-04-16 18:08:06 +0900

    project_ctbMarket

---
+++ ctbmarket/src/main/java/com/example/ctbmarket/CtbmarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.ctbmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtbmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtbmarketApplication.class, args);
+	}
+
+}
